8e99bbe351a46e6f5e1c6e4431cf4c6f4ecc6f01
devcoso <cosowebdeveloper@gmail.com>
2024-03-23 20:43:04 -0600

    Creando App, con Login y Register

---
+++ src/models/User.py
@@ -0,0 +1,25 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User () :
+    def __init__(self, id, name, email, password, points=0, remember_token=''):
+        self.id = id
+        self.name = name
+        self.email = email
+        self.password = password
+        self.points = points
+        self.remember_token = remember_token
+        
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+    def hash_password(self, password):
+        return generate_password_hash(password, method='pbkdf2')
+    def verify_user(self):
+        if(len(self.password) < 8 or len(self.password) > 16):
+            return False
+        if(len(self.name) < 3 or len(self.name) > 61):
+            return False
+        if(len(self.email) > 61):
+            return False
+        return True
+    
+# print(generate_password_hash('12345678', method='pbkdf2'))
